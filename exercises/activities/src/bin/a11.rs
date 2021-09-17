1ca4ca97d143515d14e714de5944e66ae8d5ebd2
Saúl Sáyago <sayagoadd@gmail.com>
2021-09-17 18:17:40 -0500

    Actividad a11 finalizada

--- exercises/activities/src/bin/a11.rs
+++ exercises/activities/src/bin/a11.rs
@@ -9,4 +9,24 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Item {
+    quantity: i32,
+    id: i32,
+}
+
+fn display_quantity(g_item: &Item) {
+    println!("Quantity: {:?}", g_item.quantity);
+}
+
+fn display_id(g_item: &Item) {
+    println!("Id: {:?}", g_item.id);
+}
+
+fn main() {
+    let g_item = Item {
+        quantity: 10,
+        id: 777,
+    };
+    display_quantity(&g_item);
+    display_id(&g_item);
+}
