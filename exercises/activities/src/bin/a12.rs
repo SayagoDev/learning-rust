1adeadc301799ce7a681231ad32d3b5c45c65ff1
Saúl Sáyago <sayagoadd@gmail.com>
2021-09-17 19:26:50 -0500

    Actividad a12 (impl keyword) finalizada

--- exercises/activities/src/bin/a12.rs
+++ exercises/activities/src/bin/a12.rs
@@ -10,4 +10,66 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Yellow,
+    White,
+    Green,
+    Blue,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Yellow => println!("color: yellow"),
+            Color::White => println!("color: white"),
+            Color::Green => println!("color: green"),
+            Color::Blue => println!("color: blue"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64,
+    length: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("length: {:?}", self.length);
+    }
+}
+
+struct ShippingBox {
+    weight: f64,
+    color: Color,
+    dimensions: Dimensions,
+}
+
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("weight: {:?}", self.weight);
+    }
+}
+
+
+fn main() {
+    let small_dimensions = Dimensions {
+        width: 1.0,
+        height: 5.5,
+        length: 2.0,
+    };
+    let small_box = ShippingBox::new(5.0, Color::Yellow, small_dimensions);
+    small_box.print();
+}
