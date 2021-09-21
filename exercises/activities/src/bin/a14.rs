95bfc7c23516dea7c0cfd4c940b38b3709187424
Saúl Sáyago <sayagoadd@gmail.com>
2021-09-20 20:09:54 -0500

    Actividad a14 (Strings) finalizada

--- exercises/activities/src/bin/a14.rs
+++ exercises/activities/src/bin/a14.rs
@@ -11,4 +11,40 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String,
+}
+
+fn print_data(name: &str, color: &str) {
+    println!("Name: {:?}", name);
+    println!("Favorite color: {:?}", color);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            age: 9,
+            name: "Angel".to_owned(),
+            fav_color: "Green".to_owned(),
+        },
+        Person {
+            age: 17,
+            name: "Carlos".to_owned(),
+            fav_color: "Red".to_owned(),
+        },
+        Person {
+            age: 10,
+            name: String::from("Luis"),
+            fav_color: String::from("Blue"),
+        },
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            println!("Age: {:?}", person.age);
+            print_data(&person.name, &person.fav_color);
+        }
+    }
+}
