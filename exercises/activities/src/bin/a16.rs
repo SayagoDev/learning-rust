c31564a5b08d1542573591fbc318579336de0965
Saúl Sáyago <sayagoadd@gmail.com>
2021-09-21 15:51:02 -0500

    Actividad a16 (Option) finalizada

--- exercises/activities/src/bin/a16.rs
+++ exercises/activities/src/bin/a16.rs
@@ -8,7 +8,19 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
 
-fn main() {}
+fn main() {
+    let saul = Student {
+        name: "Saul Sayago".to_owned(),
+        locker: Some(232),
+    };
+    println!("name: {:?}", saul.name);
+    match saul.locker {
+        Some(num) => println!("locker number: {:?}", num),
+        None => println!("no locker assigned"),
+    }
+}
