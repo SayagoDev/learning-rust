cfb117d8d2767827be9c4a1726c28ce92b10b14c
Saúl Sáyago <sayagoadd@gmail.com>
2021-12-19 00:35:59 -0600

    Actividad a21 (Map combinator) Finalizada

--- exercises/activities/src/bin/a21.rs
+++ exercises/activities/src/bin/a21.rs
@@ -25,4 +25,13 @@ fn find_user(name: &str) -> Option<i32> {
     }
 }
 
-fn main() {}
+fn main() {
+    let user_name = "sam";
+    let user = find_user(user_name).map(|user_id| User {
+        user_id, name: user_name.to_owned()
+    });
+    match user {
+        Some(user) => println!("{:?}", user),
+        None => println!("user not found"),
+    }
+}
